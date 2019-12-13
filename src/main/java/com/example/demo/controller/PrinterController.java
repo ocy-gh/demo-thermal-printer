86b861912a930a182395f64b7ca7f3b6fb4efd99
ocy-gh <cyo95.ong@gmail.com>
2019-12-13 16:24:49 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/PrinterController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PrinterController {
+
+    @GetMapping("/printqr")
+    public String printqr(Model model){
+        String ab = "hoelddddddddddddddd455ddddddddddddddddde";
+        model.addAttribute("a",ab);
+        return "printQR";
+    }
+
+//    @PostMapping("/print")
+//    @ResponseBody
+//    public JSONObject print(@RequestBody JSONObject requestBodyJO){
+//        System.out.println("****************************");
+//        System.out.println(requestBodyJO.toString());
+//
+////        model.addAttribute("requestBodyJO", requestBodyJO);
+//        return requestBodyJO;
+//    }
+}
